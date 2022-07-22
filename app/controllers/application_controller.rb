53df588f069f7bd5823f6d6d55f46a491c1aa09c
Rajat <rajatrkgupta@gmail.com>
2022-07-23 01:39:22 +0530

    Only Post request allowed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request!
+
+  def authenticate_request!
+    # Allow only POST HTTP verb
+    if request.method != "POST"
+      render json: { error: "Method Not Allowed", status: 405 }
+    end
+  end
 end
